81a349aae76ad411b1d0d2fc6a4c11d1bcf3f274
Harry Grillo <redbassett@gmail.com>
2013-03-25 15:30:10 -0400

    .

--- component_js.js
+++ component_js.js
@@ -1,4 +1,4 @@
-/* Version 0.1.1 testingasndsanand */
+/* Version 0.1.1 testingasndsanandandagain */
 com_redbassett_mod_hide = {
     options: {
         groupsAllow: proboards.plugin.get("mod_hide_action").settings.groupsAllow,
